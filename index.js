59cefea593adda1d53f51f8ecc574adea2341e0c
mlucasdam <damiani.lucasm@gmail.com>
2022-07-29 02:25:38 -0300

    corrigindo o servidor

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const PORT = process.env.PORT || 3000;
 const hostName = "127.0.0.1"
 
 app.listen(PORT, () => {
-    console.log(`servidor rodando em http://${hostName}:${door}`);
+    console.log(`servidor rodando em http://${hostName}:${PORT}`);
 })
 
 
