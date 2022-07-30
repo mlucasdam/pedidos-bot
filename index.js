eee35b34d63b6c3a1df07efbc2a82160bdcb3f55
mlucasdam <damiani.lucasm@gmail.com>
2022-07-30 01:38:56 -0300

    Criando a rota webhook

--- index.js
+++ index.js
@@ -2,43 +2,19 @@ const express = require("express")
 const bodyParser = require ("body-parser");
 
 const app = express ();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
     res.send("Olá chatbot");
 })
 
-app.get('/pergunta', (req,res) => {
-    msg = req.query.pergunta
-    res.send("Você preguntou: " + msg);
+app.post('/webhook', (req, res) => {
+    console.log("Cheguei no webhook")
+
+    console.log("body", req.body.queryResult);
 })
-
-app.get('/mensagem/:tipo/:id', (req, res) =>{
-    msg=req.params.tipo;
-    cod=req.params.id
-    res.send("voce quer editar o id#" + cod)
-} )
-
-
-app.post('/pedido', (req,res) => {
-    console.log(req.body)
-    const produto = req.body.produto
-    const qtd = req.body.quantidade
-    const pgto = req.body.pagamento
-    const bebida = req.body.bebida
-    
-    
-    const pedido = {
-        produto,
-        qtd,
-        pgto,
-        bebida
-    }
-
-    res.json(pedido)
-})
-
 
 const PORT = process.env.PORT || 3000;
 const hostName = "127.0.0.1"
@@ -47,4 +23,62 @@ app.listen(PORT, () => {
     console.log(`servidor rodando em http://${hostName}:${PORT}`);
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/pergunta', (req,res) => {
+//     msg = req.query.pergunta
+//     res.send("Você preguntou: " + msg);
+// })
+
+// app.get('/mensagem/:tipo/:id', (req, res) =>{
+//     msg=req.params.tipo;
+//     cod=req.params.id
+//     res.send("voce quer editar o id#" + cod)
+// } )
+
+
+// app.post('/pedido', (req,res) => {
+//     console.log(req.body)
+//     const produto = req.body.produto
+//     const qtd = req.body.quantidade
+//     const pgto = req.body.pagamento
+//     const bebida = req.body.bebida
+    
+    
+//     const pedido = {
+//         produto,
+//         qtd,
+//         pgto,
+//         bebida
+//     }
+
+//     res.json(pedido)
+// })
 
