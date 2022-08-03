4ce20f3307cf25cdaf745dcd336047de2dc85c18
mlucasdam <damiani.lucasm@gmail.com>
2022-08-03 01:22:19 -0300

    corrigindo erro do import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bodyParser = require ("body-parser");
-const Model = require("../model")
+const Model = require("./model")
 
 const app = express ();
 app.use(bodyParser.urlencoded({extended: false}));
