d3faac1a003d04d762bfc1aff7d54a6dba977444
mlucasdam <damiani.lucasm@gmail.com>
2022-08-04 00:56:58 -0300

    introduzindo a resposta por imagem

--- model/index.js
+++ model/index.js
@@ -1,7 +1,17 @@
 exports.VerCardapio = (msg, params) => {
     let resposta = {
+        tipo: 'imagem',
+        url: 'http://lorempixel.com.br/500/500/?2'
+    }
+    return resposta
+}
+
+// http://lorempixel.com.br/500/500/?2
+
+exports.verStatus = (msg, params) => {
+    let resposta = {
         tipo: 'texto',
-        mensagem: ' olá de dentro da model'
+        mensagem: 'Calma o seu pedido ja está sendo prepardo!!'
     }
 
     return resposta
